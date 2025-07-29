e71e1c2b85c4baf978f4fd2844fc81a4d78f91da
Joyy75 <thirithalsuhlaing@gmail.com>
2025-07-29 18:37:10 +0630

    Finalize

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,38 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className='bg-black text-white px-8 md:px-16 lg:px-24'>
-        <div className='container py-2 flex justify-center md:justify-between items-center'>
-            <div className='text-2xl font-bold hidden md:inline'>Joyy</div>
-            <div className='space-x-6'>
-                <a href="#home" className='hover:text-gray-400'>Home</a>
-                <a href="#about" className='hover:text-gray-400'>About Me</a>
-                <a href="#service" className='hover:text-gray-400'>Services</a>
-                <a href="#project" className='hover:text-gray-400'>Projects</a>
-                <a href="#contact" className='hover:text-gray-400'>Contact</a>
-            </div>
-            <a href="https://www.instagram.com/accounts/login/?next=https%3A%2F%2Fwww.instagram.com%2Fyes_im_joyy%2F%3Figsh%3DM21uam15Ynpya2oy%26utm_source%3Dqr&is_from_rle">
-            <button className='bg-gradient-to-r mt-4 from-green-400 to-blue-500 text-white hidden md:inline
-            transform transition-transform duration-300 hover:scale-90 px-4 py-2 rounded-full'>Connect Me</button>
-            </a>
+    <nav className="bg-black text-white px-4 sm:px-8 md:px-16">
+      <div className="max-w-7xl mx-auto py-4 flex flex-wrap justify-between items-center">
+        <div className="text-2xl font-bold md:text-4xl">Joyy</div>
+        <div className="flex flex-row flex-wrap items-center gap-4">
+          <a href="#home" className="hover:text-gray-400">
+            Home
+          </a>
+          <a href="#about" className="hover:text-gray-400">
+            About Me
+          </a>
+          <a href="#service" className="hover:text-gray-400">
+            Services
+          </a>
+          <a href="#project" className="hover:text-gray-400">
+            Projects
+          </a>
+          <a href="#contact" className="hover:text-gray-400">
+            Contact
+          </a>
         </div>
-    </nav>
-  )
-}
 
-export default Navbar
+        <div className="hidden md:block">
+          <a href="https://www.instagram.com/accounts/login/?next=https%3A%2F%2Fwww.instagram.com%2Fyes_im_joyy%2F%3Figsh%3DM21uam15Ynpya2oy%26utm_source%3Dqr&is_from_rle">
+            <button className="bg-gradient-to-r from-green-400 to-blue-500 text-white px-4 py-2 rounded-full transform transition-transform duration-300 hover:scale-90 sm: ">
+              Connect Me
+            </button>
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
